35114e9fb0acbfb8aa30802617cf8b3ccc1e08f2
hamedbanifatemi <37498856+hamedbanifatemi@users.noreply.github.com>
2022-07-05 08:19:04 +0430

    Create netconf-1.py

---
+++ netconf-1.py
@@ -0,0 +1,25 @@
+import warnings
+import xml.dom.minidom
+
+warnings.simplefilter("ignore", DeprecationWarning)
+
+from ncclient import manager
+
+m = manager.connect(host='172.17.3.3',
+                    port=830, # 830
+                    username='admin',
+                    password='123',
+                    device_params={'name': 'csr'})
+
+netconf_filter = '''
+<filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+    <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native" />
+</filter>
+'''
+
+reply = m.get(netconf_filter)
+print(reply)
+
+config = xml.dom.minidom.parseString(reply.xml)
+print(config.toprettyxml())
+
